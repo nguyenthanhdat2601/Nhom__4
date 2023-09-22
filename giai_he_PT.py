4dbf323e7d42e36287986725e3e12a4dee3f9eae
Nguyễn Văn Thơ <nguyenvanthohaui@gmail.com>
2023-09-22 08:16:39 +0700

    3rd commi: tao GUI

--- giai_he_PT.py
+++ giai_he_PT.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def Input1():
     #so phuong trinh
-    sopt = int(input("Nhap so phuong trinh cua he: "))
-    return sopt
+    sopt = IntVar()
+    return sopt.get()
 def Input2():
     #so an 
-    soan = int(input("nhap so an cua phuong trinh: "))
-    return soan
+    soan = IntVar()
+    return soan.get()
 def test_input(sopt, soan):
     if (sopt != soan or sopt < 0 or soan < 0):
             print("Error! Nhap lai!")
@@ -56,5 +56,75 @@ def selection():
     else: 
         print("Ban da nhap sai lua chon, vui long nhap lai!")
         selection()
-selection()
+from tkinter import *
+from tkinter import messagebox
+
+win = Tk()
+win.title("giai he co n phuong trinh va n an")
+win.geometry('900x700')
+win.configure(bg = 'purple')
+
+Label(win, text = 'Giai he phuong trinh n an', font = ('Arial', 13), bg = 'yellow', fg = 'black', height=2, relief='solid', borderwidth = 2, width = 40).place(x = 250, y = 0)
+
+sopt = IntVar()
+Label(win, text = 'Nhap so phuong trinh', font = ('Arial', 9), bg = 'yellow', fg = 'black', height=1, relief='solid', borderwidth = 2, width = 30).place(x = 250, y = 60)
+Entry(win, textvariable = sopt, font = ('Arial', 13), bg = 'yellow', fg = 'black', relief='solid', borderwidth = 2, width = 5).place(x = 485, y = 60)
+
+def create():
+    A = []
+    entries_A = []
+    B = []
+    entries_B = []
+    soan = sopt.get()
+
+    matrix_A = []
+    matrix_B = []
+
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    a1 = list(a)
+    for i in range(sopt.get()):
+        A.append([])
+        entries_A.append([])
+        B.append([])
+        entries_B.append([])
+        matrix_A.append([])
+        matrix_B.append([])
+        for j in range(soan):
+            A[i].append(DoubleVar())
+            if (i > 0):
+                Label(win,background = 'purple', text = ' + '+a1[j]+f'{i}', width = 3).place(x = 250 + 80*i, y = 110 + 30*j)#grid(row = i, column = j+2)
+            else:
+                Label(win,background = 'purple', text = a1[j]+f'{i}', width = 3).place(x = 250 + 80*i, y = 110 + 30*j)#grid(row = i, column = j+2)
+            entries_A[i].append(Entry(win, textvariable = A[i][j], width=3))
+            entries_A[i][j].place(x =280 + 83*j, y = 110 + 30*i)#grid(row=i, column=j+4)
+            matrix_A[i].append(A[i][j].get())
+        B[i].append(DoubleVar())
+        Label(win,background = 'purple', text = f' = ', width = 3).place( x = 310 + 83*(soan-1), y = 110 + 30*i)#grid(row = i, column = 4)
+        entries_B[i].append(Entry(win, textvariable = B[i][0], width=3))
+        entries_B[i][0].place( x = (soan+1)*128, y = 110 + 30*i)#grid(row= i, column= 5)
+        matrix_B[i].append(B[i][0].get())
+
+    matrix_A = np.array(matrix_A)
+    matrix_B = np.array(matrix_B)
+    print(matrix_A)
+    return matrix_A, matrix_B
+    
+def clear():
+    Label(win,background = 'purple', width = 180*sopt.get(), height = 20).place( x = 240, y = 100)
+A, B = create()
+def calculate():
+    print(A)
+    if (np.linalg.det(A) != 0):
+        X = np.dot(np.linalg.inv(A), B)
+        for i in range(sopt.get()):
+            Label(win,background = 'purple', text = f'x{i} = ', width = 3).place( x = 50 + 83*i, y = 450)#grid(row = i, column = 4)
+            Label(win,background = 'purple', text = f'{X[i][0]} ', width = 3).place( x = 100 + 83*i, y = 450)#grid(row = i, column = 4)
+
+Button(win, text = 'Create', command = create, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 280, y = 510)
+Button(win, text = 'Exit', command = exit, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 350, y = 510)
+Button(win, text = 'Clear', command = clear, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 420, y = 510)
+Button(win, text = 'Calculate', command = calculate, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 490, y = 510)
+
+win.mainloop()
+#selection()
 
