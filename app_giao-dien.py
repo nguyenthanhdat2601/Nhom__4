0cf64fe7cdc2a90f8ad8c1ceb01e07679f3bc5b0
Trinh Hoang Long <trinhhoanglong15052003@gmail.com>
2023-09-22 20:24:59 +0700

    update gui

--- app_giao-dien.py
+++ app_giao-dien.py
@@ -1,85 +1,130 @@
 import numpy as np
-import tkinter as tk
-from tkinter import ttk
 
 def Input1():
-    sopt = int(input("Nhap so phuong trinh cua he: "))
-    return sopt
-
+    #so phuong trinh
+    sopt = IntVar()
+    return sopt.get()
 def Input2():
-    soan = int(input("Nhap so an cua phuong trinh: "))
-    return soan
-
+    #so an 
+    soan = IntVar()
+    return soan.get()
 def test_input(sopt, soan):
-    if sopt != soan or sopt < 0 or soan < 0:
-        print("Error! Nhap lai!")
-        sopt = Input1()
-        soan = Input2()
-        test_input(sopt, soan)
-    else:
+    if (sopt != soan or sopt < 0 or soan < 0):
+            print("Error! Nhap lai!")
+            sopt = Input1()
+            soan = Input2()
+            test_input(sopt, soan)
+    else: 
         return sopt, soan
-
 def Input3(sopt, soan):
-    A = np.zeros((sopt, soan), dtype=float)
-    B = np.zeros((sopt, 1), dtype=float)
-    for pt in range(0, sopt):
-        print(f"Nhap he so cua phuong trinh thu {pt + 1}: ")
-        for an in range(0, sopt):
-            A[pt, an] = float(input(f"----He so thu {an + 1}: "))
-        B[pt, 0] = float(input("----He so cuoi: "))
-    print(A)
-    print(B)
-    return A, B
-
+        A = np.zeros((sopt, soan), dtype = float)
+        B = np.zeros((sopt, 1), dtype = float)
+        for pt in range(0, sopt):
+            print(f"Nhap he so cua phuong trinh thu {pt + 1}: ")
+            for an in range(0, sopt):
+                A[pt, an] = float(input(f"----he so thu {an + 1}: "))
+            B[pt, 0] = float(input("----he so cuoi: "))
+        print (A)
+        print (B)
+        return A, B
 def calculate(A, B):
     try:
-        if np.linalg.det(A) != 0:
+        if (np.linalg.det(A) != 0):
             X = np.dot(np.linalg.inv(A), B)
             return X
-        else:
+        else: 
             calculate(A, B)
     except:
         print("Error!")
         Input3(A, B)
         calculate()
 
-def continue_button():
-    sopt = int(input1_entry.get())
-    soan = int(input2_entry.get())
-    sopt, soan = test_input(sopt, soan)
-    A, B = Input3(sopt, soan)
-    X = calculate(A, B)
-    result_label.config(text=f'Nghiem cua he: {X}')
+def selection():
+    opinion = input("Nhap vao lua chon: Continue or Exit? ")
+    if (opinion == "Continue"):
+        sopt = Input1()
+        soan = Input2()
+        sopt, soan = test_input(sopt, soan)
+        A, B = Input3(sopt, soan)
+        X = calculate(A, B)
+        print('Nghiem cua he:',X)
+        print("=====================")
+        selection()
+    elif (opinion == "Exit"): 
+        print("Thank for using!")
+        exit()
+    else: 
+        print("Ban da nhap sai lua chon, vui long nhap lai!")
+        selection()
+from tkinter import *
+from tkinter import messagebox
 
-def Exit_button():
-    print("Cam on!")
-    root.destroy()
+win = Tk()
+win.title("giai he co n phuong trinh va n an")
+win.geometry('900x700')
+win.configure(bg = 'purple')
 
-root = tk.Tk()
-root.title("Giai he phuong trinh tuyen tinh")
-root.geometry('500x400')
-frame = ttk.Frame(root)
-frame.grid(column=0, row=0, padx=10, pady=10)
+Label(win, text = 'Giai he phuong trinh n an', font = ('Arial', 13), bg = 'yellow', fg = 'black', height=2, relief='solid', borderwidth = 2, width = 40).place(x = 250, y = 0)
 
-input1_label = ttk.Label(frame, text="Nhap so phuong trinh cua he:")
-input1_label.grid(column=0, row=0, padx=5, pady=5)
+sopt = IntVar()
+Label(win, text = 'Nhap so phuong trinh', font = ('Arial', 9), bg = 'yellow', fg = 'black', height=1, relief='solid', borderwidth = 2, width = 30).place(x = 250, y = 60)
+Entry(win, textvariable = sopt, font = ('Arial', 13), bg = 'yellow', fg = 'black', relief='solid', borderwidth = 2, width = 5).place(x = 485, y = 60)
 
-input1_entry = ttk.Entry(frame)
-input1_entry.grid(column=1, row=0, padx=5, pady=5)
+def create():
+    A = []
+    entries_A = []
+    B = []
+    entries_B = []
+    soan = sopt.get()
 
-input2_label = ttk.Label(frame, text="Nhap so an cua phuong trinh:")
-input2_label.grid(column=0, row=1, padx=5, pady=5)
+    matrix_A = []
+    matrix_B = []
 
-input2_entry = ttk.Entry(frame)
-input2_entry.grid(column=1, row=1, padx=5, pady=5)
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    a1 = list(a)
+    for i in range(sopt.get()):
+        A.append([])
+        entries_A.append([])
+        B.append([])
+        entries_B.append([])
+        matrix_A.append([])
+        matrix_B.append([])
+        for j in range(soan):
+            A[i].append(DoubleVar())
+            if (i > 0):
+                Label(win,background = 'purple', text = ' + '+a1[j]+f'{i}', width = 3).place(x = 250 + 80*i, y = 110 + 30*j)#grid(row = i, column = j+2)
+            else:
+                Label(win,background = 'purple', text = a1[j]+f'{i}', width = 3).place(x = 250 + 80*i, y = 110 + 30*j)#grid(row = i, column = j+2)
+            entries_A[i].append(Entry(win, textvariable = A[i][j], width=3))
+            entries_A[i][j].place(x =280 + 83*j, y = 110 + 30*i)#grid(row=i, column=j+4)
+            matrix_A[i].append(A[i][j].get())
+        B[i].append(DoubleVar())
+        Label(win,background = 'purple', text = f' = ', width = 3).place( x = 310 + 83*(soan-1), y = 110 + 30*i)#grid(row = i, column = 4)
+        entries_B[i].append(Entry(win, textvariable = B[i][0], width=3))
+        entries_B[i][0].place( x = (soan+1)*128, y = 110 + 30*i)#grid(row= i, column= 5)
+        matrix_B[i].append(B[i][0].get())
 
-continue_button = ttk.Button(frame, text="Tiep tuc", command=continue_button)
-continue_button.grid(column=0, row=2, padx=5, pady=5)
+    matrix_A = np.array(matrix_A)
+    matrix_B = np.array(matrix_B)
+    print(matrix_A)
+    return matrix_A, matrix_B
+    
+def clear():
+    Label(win,background = 'purple', width = 180*sopt.get(), height = 20).place( x = 240, y = 100)
+A, B = create()
+def calculate():
+    print(A)
+    if (np.linalg.det(A) != 0):
+        X = np.dot(np.linalg.inv(A), B)
+        for i in range(sopt.get()):
+            Label(win,background = 'purple', text = f'x{i} = ', width = 3).place( x = 50 + 83*i, y = 450)#grid(row = i, column = 4)
+            Label(win,background = 'purple', text = f'{X[i][0]} ', width = 3).place( x = 100 + 83*i, y = 450)#grid(row = i, column = 4)
 
-exit_button = ttk.Button(frame, text="Thoat", command=Exit_button)
-exit_button.grid(column=1, row=2, padx=5, pady=5)
+Button(win, text = 'Create', command = create, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 280, y = 510)
+Button(win, text = 'Exit', command = exit, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 350, y = 510)
+Button(win, text = 'Clear', command = clear, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 420, y = 510)
+Button(win, text = 'Calculate', command = calculate, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 490, y = 510)
 
-result_label = ttk.Label(frame, text="")
-result_label.grid(column=0, row=3, columnspan=2, padx=5, pady=5)
+win.mainloop()
+#selection()
 
-root.mainloop()
