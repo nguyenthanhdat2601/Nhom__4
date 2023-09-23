a2b9611ff36dc925b5acd87ca79cd4734c8334f4
Nguyễn Văn Thơ <nguyenvanthohaui@gmail.com>
2023-09-24 00:10:44 +0700

    6th commit: - hoan thien chuc nang refresh bang ham clear

--- giai_he_PT.py
+++ giai_he_PT.py
@@ -4,14 +4,14 @@ from tkinter import messagebox
 
 win = Tk()
 win.title("giai he co n phuong trinh va n an")
-win.geometry('900x700')
+win.geometry('900x900')
 win.configure(bg = 'purple')
 
-Label(win, text = 'Giai he phuong trinh n an', font = ('Arial', 13), bg = 'yellow', fg = 'black', height=2, relief='solid', borderwidth = 2, width = 40).place(x = 250, y = 0)
+Label(win, text = 'Giải hệ phương trình n ẩn', font = ('Arial', 13), bg = 'yellow', fg = 'black', height=2, relief='solid', borderwidth = 2, width = 40).place(x = 250, y = 0)
 
-sopt = IntVar()
-Label(win, text = 'Nhap so phuong trinh', font = ('Arial', 9), bg = 'yellow', fg = 'black', height=1, relief='solid', borderwidth = 2, width = 30).place(x = 250, y = 60)
-Entry(win, textvariable = sopt, font = ('Arial', 13), bg = 'yellow', fg = 'black', relief='solid', borderwidth = 2, width = 5).place(x = 485, y = 60)
+sopt = StringVar()
+Label(win, text = 'Nhập số phương trình', font = ('Arial', 13), bg = 'yellow', fg = 'black', height=2, relief='solid', borderwidth = 2, width = 30).place(x = 250, y = 60)
+Entry(win, textvariable = sopt, font = ('Arial', 14), bg = 'yellow', fg = 'black', relief='solid', borderwidth = 2, width = 5).place(x = 555, y = 60)
 
 a = 'abcdefghijklmnopqrstuvwxyz'
 a1 = list(a)
@@ -27,33 +27,33 @@ class Cal:
         self.entries_B = entries_B
     def create(self):
         try:
-            if (sopt.get() <= 0):
+            if (int(sopt.get()) <= 0):
                 messagebox.showwarning("CHu y","So phuong trinh phai >0")
-            for i in range(sopt.get()):
+            for i in range(int(sopt.get())):
                 self.A.append([])
                 self.entries_A.append([])
                 self.B.append([])
                 self.entries_B.append([])
-                for j in range(sopt.get()):
+                for j in range(int(sopt.get())):
                     self.A[i].append(StringVar())
-                    if (i > 0):
-                        Label(win,background = 'purple', text = ' + '+a1[j]+f'{i}', width = 3).place(x = 80 + 80*i, y = 200 + 30*j)#grid(row = i, column = j+2)
+                    if (i != 0):
+                        Label(win,background = 'purple', font = ('Arial', 12), text = ' + '+a1[j]+f'{i} ', width = 3).place(x = 250 + 80*i, y = 110 + 30*j)#grid(row = i, column = j+2)
                     else:
-                        Label(win,background = 'purple', text = a1[j]+f'{i}', width = 3).place(x = 80 + 80*i, y = 200 + 30*j)#grid(row = i, column = j+2)
+                        Label(win,background = 'purple', font = ('Arial', 12), text = a1[j]+f'{i} ', width = 3).place(x = 250 + 80*i, y = 110 + 30*j)#grid(row = i, column = j+2)
                     self.entries_A[i].append(Entry(win, textvariable = self.A[i][j], width=3))
-                    self.entries_A[i][j].place(x =110 + 83*j, y = 200 + 30*i)#grid(row=i, column=j+4)
+                    self.entries_A[i][j].place(x =280 + 83*j, y = 110 + 30*i)#grid(row=i, column=j+4)
                 self.B[i].append(StringVar())
-                Label(win,background = 'purple', text = f' = ', width = 3).place( x = 140 + 83*2, y = 200 + 30*i)#grid(row = i, column = 4)
+                Label(win,background = 'purple', font = ('Arial', 12), text = f' = ', width = 3).place( x = 310 + 83*(int(sopt.get())-1), y = 110 + 30*i)#grid(row = i, column = 4)
                 self.entries_B[i].append(Entry(win, textvariable = self.B[i][0], width=3))
-                self.entries_B[i][0].place( x = 83*4, y = 200 + 30*i)#grid(row= i, column= 5)
+                self.entries_B[i][0].place(x=200 + 83 * (int(sopt.get())+1), y=110 + 30 * i)
         except:
             messagebox.showwarning("CHu y","Kieu du lieu khong dung!")
 
     def get_mat_A(self):
         matrix_A = []
-        for i3 in range(sopt.get()):
+        for i3 in range(int(sopt.get())):
             matrix_A.append([])
-            for j3 in range(sopt.get()):
+            for j3 in range(int(sopt.get())):
                 if (self.A[i3][j3].get() != None):
                     matrix_A[i3].append(float(self.A[i3][j3].get()))
                 else: 
@@ -63,7 +63,7 @@ class Cal:
 
     def get_mat_B(self):
         matrix_B = []
-        for i3 in range(sopt.get()):
+        for i3 in range(int(sopt.get())):
             matrix_B.append([])
             for j3 in range(1):
                 if (self.B[i3][j3].get() != None):
@@ -78,20 +78,33 @@ class Cal:
             B = Cal.get_mat_B(self)
             if (np.linalg.det(A) != 0):
                 X = np.dot(np.linalg.inv(A), B)
-                for i in range(sopt.get()):
-                    Label(win,background = 'purple', text = f'x{i} = ', width = 4).place( x = 50 + 83*i, y = 450)#grid(row = i, column = 4)
-                    Label(win,background = 'purple', text = f'{round(X[i][0], 2)}' + ', ', width = 4).place( x = 100 + 83*i, y = 450)#grid(row = i, column = 4)
+                for i in range(int(sopt.get())):
+                    
+                    Label(win,background = 'purple', font = ('Arial', 12), fg = 'White', text = f'x{i} = ', width = 4).place( x = 280 + 83*i, y = 650)#grid(row = i, column = 4)
+                    Label(win,background = 'purple', font = ('Arial', 12), fg = 'White', text = f'{round(X[i][0], 2)}' + ', ', width = 4).place( x = 320 + 83*i, y = 650)#grid(row = i, column = 4)
             else:
                 messagebox.showwarning("Error!", "Ma tran khong kha nghich")
         except:
             messagebox.showwarning("Error!", "Nhap sai!")
 def clear():
-    Label(win,background = 'purple', width = 180*sopt.get(), height = 20).place( x = 240, y = 100)
+    Label(win,background = 'purple', width = 180*int(sopt.get()), height = 20).place( x = 240, y = 100)
+    Label(win,background = 'purple', width = 100).place( x = 280, y = 650)
+    A = []
+    entries_A = []
+    B = []
+    entries_B = []
+    cal = Cal(A, entries_A, B, entries_B)
+
+    Button(win, text = 'Create', command = cal.create, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 280, y = 710)
+    Button(win, text = 'Exit', command = exit, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 380, y = 710)
+    Button(win, text = 'Clear', command = clear, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 480, y = 710)
+    Button(win, text = 'Calculate', command = cal.calculate, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 8, height = 2, relief='solid', borderwidth = 2).place(x = 580, y = 710)
+    
 cal = Cal(A, entries_A, B, entries_B)
-Button(win, text = 'Create', command = cal.create, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 280, y = 510)
-Button(win, text = 'Exit', command = exit, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 380, y = 510)
-Button(win, text = 'Clear', command = clear, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 480, y = 510)
-Button(win, text = 'Calculate', command = cal.calculate, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 580, y = 510)
+Button(win, text = 'Create', command = cal.create, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 280, y = 710)
+Button(win, text = 'Exit', command = exit, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 380, y = 710)
+Button(win, text = 'Clear', command = clear, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 480, y = 710)
+Button(win, text = 'Calculate', command = cal.calculate, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 8, height = 2, relief='solid', borderwidth = 2).place(x = 580, y = 710)
 
 win.mainloop()
 
