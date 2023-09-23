3859745c7578669e1cbfd08e0857109e17e9836d
Nguyễn Văn Thơ <nguyenvanthohaui@gmail.com>
2023-09-23 12:12:40 +0700

    4th commit: hoan thien chuc nang tinh toan

--- giai_he_PT.py
+++ giai_he_PT.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def Input1():
+""" def Input1():
     #so phuong trinh
     sopt = IntVar()
     return sopt.get()
@@ -37,25 +37,8 @@ def calculate(A, B):
     except:
         print("Error!")
         Input3(A, B)
-        calculate()
+        calculate() """
 
-def selection():
-    opinion = input("Nhap vao lua chon: Continue or Exit? ")
-    if (opinion == "Continue"):
-        sopt = Input1()
-        soan = Input2()
-        sopt, soan = test_input(sopt, soan)
-        A, B = Input3(sopt, soan)
-        X = calculate(A, B)
-        print('Nghiem cua he:',X)
-        print("=====================")
-        selection()
-    elif (opinion == "Exit"): 
-        print("Thank for using!")
-        exit()
-    else: 
-        print("Ban da nhap sai lua chon, vui long nhap lai!")
-        selection()
 from tkinter import *
 from tkinter import messagebox
 
@@ -70,61 +53,74 @@ sopt = IntVar()
 Label(win, text = 'Nhap so phuong trinh', font = ('Arial', 9), bg = 'yellow', fg = 'black', height=1, relief='solid', borderwidth = 2, width = 30).place(x = 250, y = 60)
 Entry(win, textvariable = sopt, font = ('Arial', 13), bg = 'yellow', fg = 'black', relief='solid', borderwidth = 2, width = 5).place(x = 485, y = 60)
 
-def create():
-    A = []
-    entries_A = []
-    B = []
-    entries_B = []
-    soan = sopt.get()
+A = []
+entries_A = []
+B = []
+entries_B = []
+class Cal:
+    def __init__(self, A, entries_A, B, entries_B):
+        self.A = A
+        self.entries_A = entries_A
+        self.B = B
+        self.entries_B = entries_B
+    def create(self):
+        a = 'abcdefghijklmnopqrstuvwxyz'
+        a1 = list(a)
+        for i in range(sopt.get()):
+            self.A.append([])
+            self.entries_A.append([])
+            self.B.append([])
+            self.entries_B.append([])
+            for j in range(sopt.get()):
+                self.A[i].append(DoubleVar())
+                if (i > 0):
+                    Label(win,background = 'purple', text = ' + '+a1[j]+f'{i}', width = 3).place(x = 80 + 80*i, y = 200 + 30*j)#grid(row = i, column = j+2)
+                else:
+                    Label(win,background = 'purple', text = a1[j]+f'{i}', width = 3).place(x = 80 + 80*i, y = 200 + 30*j)#grid(row = i, column = j+2)
+                self.entries_A[i].append(Entry(win, textvariable = self.A[i][j], width=3))
+                self.entries_A[i][j].place(x =110 + 83*j, y = 200 + 30*i)#grid(row=i, column=j+4)
+            self.B[i].append(DoubleVar())
+            Label(win,background = 'purple', text = f' = ', width = 3).place( x = 140 + 83*2, y = 200 + 30*i)#grid(row = i, column = 4)
+            self.entries_B[i].append(Entry(win, textvariable = self.B[i][0], width=3))
+            self.entries_B[i][0].place( x = 83*4, y = 200 + 30*i)#grid(row= i, column= 5)
 
-    matrix_A = []
-    matrix_B = []
+    def get_mat_A(self):
+        matrix_A = []
+        for i3 in range(sopt.get()):
+            matrix_A.append([])
+            for j3 in range(sopt.get()):
+                matrix_A[i3].append(self.A[i3][j3].get())
 
-    a = 'abcdefghijklmnopqrstuvwxyz'
-    a1 = list(a)
-    for i in range(sopt.get()):
-        A.append([])
-        entries_A.append([])
-        B.append([])
-        entries_B.append([])
-        matrix_A.append([])
-        matrix_B.append([])
-        for j in range(soan):
-            A[i].append(DoubleVar())
-            if (i > 0):
-                Label(win,background = 'purple', text = ' + '+a1[j]+f'{i}', width = 3).place(x = 250 + 80*i, y = 110 + 30*j)#grid(row = i, column = j+2)
+        return np.array(matrix_A)
+
+    def get_mat_B(self):
+        matrix_B = []
+        for i3 in range(sopt.get()):
+            matrix_B.append([])
+            for j3 in range(1):
+                matrix_B[i3].append(self.B[i3][j3].get())
+        return np.array(matrix_B)
+
+    def calculate(self):
+        try:
+            A = Cal.get_mat_A(self)
+            B = Cal.get_mat_B(self)
+            if (np.linalg.det(A) != 0):
+                X = np.dot(np.linalg.inv(A), B)
+                for i in range(sopt.get()):
+                    Label(win,background = 'purple', text = f'x{i} = ', width = 4).place( x = 50 + 83*i, y = 450)#grid(row = i, column = 4)
+                    Label(win,background = 'purple', text = f'{round(X[i][0], 2)}' + ', ', width = 4).place( x = 100 + 83*i, y = 450)#grid(row = i, column = 4)
             else:
-                Label(win,background = 'purple', text = a1[j]+f'{i}', width = 3).place(x = 250 + 80*i, y = 110 + 30*j)#grid(row = i, column = j+2)
-            entries_A[i].append(Entry(win, textvariable = A[i][j], width=3))
-            entries_A[i][j].place(x =280 + 83*j, y = 110 + 30*i)#grid(row=i, column=j+4)
-            matrix_A[i].append(A[i][j].get())
-        B[i].append(DoubleVar())
-        Label(win,background = 'purple', text = f' = ', width = 3).place( x = 310 + 83*(soan-1), y = 110 + 30*i)#grid(row = i, column = 4)
-        entries_B[i].append(Entry(win, textvariable = B[i][0], width=3))
-        entries_B[i][0].place( x = (soan+1)*128, y = 110 + 30*i)#grid(row= i, column= 5)
-        matrix_B[i].append(B[i][0].get())
-
-    matrix_A = np.array(matrix_A)
-    matrix_B = np.array(matrix_B)
-    print(matrix_A)
-    return matrix_A, matrix_B
-    
+                messagebox.showwarning("Error!", "Ma tran khong kha nghich")
+        except:
+            messagebox.showwarning("Error!", "Nhap sai!")
 def clear():
     Label(win,background = 'purple', width = 180*sopt.get(), height = 20).place( x = 240, y = 100)
-A, B = create()
-def calculate():
-    print(A)
-    if (np.linalg.det(A) != 0):
-        X = np.dot(np.linalg.inv(A), B)
-        for i in range(sopt.get()):
-            Label(win,background = 'purple', text = f'x{i} = ', width = 3).place( x = 50 + 83*i, y = 450)#grid(row = i, column = 4)
-            Label(win,background = 'purple', text = f'{X[i][0]} ', width = 3).place( x = 100 + 83*i, y = 450)#grid(row = i, column = 4)
-
-Button(win, text = 'Create', command = create, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 280, y = 510)
-Button(win, text = 'Exit', command = exit, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 350, y = 510)
-Button(win, text = 'Clear', command = clear, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 420, y = 510)
-Button(win, text = 'Calculate', command = calculate, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 490, y = 510)
+cal = Cal(A, entries_A, B, entries_B)
+Button(win, text = 'Create', command = cal.create, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 280, y = 510)
+Button(win, text = 'Exit', command = exit, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 380, y = 510)
+Button(win, text = 'Clear', command = clear, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 480, y = 510)
+Button(win, text = 'Calculate', command = cal.calculate, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 580, y = 510)
 
 win.mainloop()
-#selection()
 
