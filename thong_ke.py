afba2e4eca7c6cc0b2fe41b42a931c130f27c91c
Nguyễn Văn Thơ <nguyenvanthohaui@gmail.com>
2023-09-26 15:29:04 +0700

    1st commit:

--- thong_ke.py
+++ thong_ke.py
@@ -5,7 +5,31 @@ import numpy as np
 df=pd.read_csv('diemPython.csv',index_col=0,header = 0)
 in_data = array(df.iloc[:,:])
 print(in_data)
-print('Tong so sinh vien di thi :')
+
+#mục 7: tổng số sinh viên đạt bài cuối kỳ
+def sv_ck(in_data):
+    print("số sinh viên đạt bài cuối kỳ: ")
+    sv_cuoi_ky = in_data[:, 15]
+    return np.sum(sv_cuoi_ky)
+
+#muc 8: so sanh so sinh vien dat chuan L1, L2
+def ipL1_l2(in_data):
+    sv_L1 = np.sum(in_data[:, 12])
+    print("so sinh vien dat chuan dau ra L1: ", sv_L1)
+    sv_L2 = np.sum(in_data[:, 13])
+    print("so sinh vien dat chuan dau ra L2: ", sv_L2)
+    if (sv_L1 > sv_L2) :
+        return "=> so sinh vien dat L1 lon hon L2"
+    elif (sv_L1 < sv_L2):
+        return "=> so sinh vien dat L2 lon hon L1"
+    else:
+        return "=> so sinh vien dat L2 bang L1"
+print(sv_ck(in_data))
+print(ipL1_l2(in_data))
+
+#muc 1: tong so sinh vien tham gia mon h
+
+""" print('Tong so sinh vien di thi :')
 tongsv= in_data[:,1]
 print(np.sum(tongsv))
 diemA = in_data[:,3]
@@ -19,5 +43,5 @@ plt.plot(range(len(diemBc)),diemBc,'g-',label="Diem B +")
 plt.xlabel('Lơp')
 plt.ylabel(' So sv dat diem ')
 plt.legend(loc='upper right')
-plt.show()
+plt.show() """
       
